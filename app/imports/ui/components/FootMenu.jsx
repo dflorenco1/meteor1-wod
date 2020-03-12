a31fb9391a309398619a3819d533e62013187ce5
dflorenco1 <60244325+dflorenco1@users.noreply.github.com>
2020-03-12 11:05:22 -1000

    Finished

---
+++ app/imports/ui/components/FootMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+        <div className="footer">
+          <Grid container>
+            <Grid.Row centered>
+              (c) 2017 Three Peaks <br/>
+              Photos taken by Aki Lily Photography. <br/>
+              Site created by ICS 314.
+            </Grid.Row>
+          </Grid>
+        </div>
+    );
+  }
+}
+
+export default FooterMenu;
---
+++ app/imports/ui/components/FullWidthImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+class FullWidthImage extends React.Component {
+  render() {
+    return (
+        <Image fluid src="//courses.ics.hawaii.edu/ics314s20/morea/ui-frameworks/wod-three-peaks-image.png"/>
+    );
+  }
+}
+
+export default FullWidthImage;
